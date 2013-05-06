2388e89298a5c9313959e7d32d59da7058321d70
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2013-05-06 19:21:11 -0300

    Empty Rails 3.2.13 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortalempresaStealth::Application.config.secret_token = 'b46e2f0534ff90fd125189c194b9f39e8cc090c6cc0a4968bf4d7014d5a8e872cd573b7d710eb143b9ae10e61967596b0a00691c5c6af34bf9fdb9bb7d29c7f2'
