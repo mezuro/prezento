d0815209e53769c4f7a2c2c55e8aacf0e7f50129
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2013-08-17 11:06:17 -0300

    New deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rails_env, "production"
 
 set :rvm_ruby_string, :local              # use the same ruby as used locally for deployment
 set :rvm_autolibs_flag, "read-only"       # more info: rvm help autolibs
-#set :rvm_type, :system
+set :rvm_type, :system
 
 set :application, "mezuro"
 set :deploy_to, "/home/mezuro/app"
@@ -18,11 +18,11 @@ set :use_sudo, false
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "ec2-54-224-114-220.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
-role :app, "ec2-54-224-114-220.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
-role :db,  "ec2-54-224-114-220.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
+role :web, "ec2-184-72-147-209.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-184-72-147-209.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
+role :db,  "ec2-184-72-147-209.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
 
-before 'deploy:setup',          'rvm:install_rvm'        # install RVM
+# before 'deploy:setup',          'rvm:install_rvm'        # install RVM
 before 'deploy:setup',          'rvm:install_ruby'       # install Ruby and create gemset, OR:
 before 'deploy:setup',          'rvm:create_gemset'      # only create gemset
 after  'deploy:assets:symlink', 'deploy:config_symlinks'
--- features/support/env.rb
+++ features/support/env.rb
@@ -10,6 +10,8 @@ SimpleCov.coverage_dir 'coverage/cucumber'
 
 require 'cucumber/rails'
 
+#require 'kalibro_entities/kalibro_cucumber_helpers/hooks'
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
