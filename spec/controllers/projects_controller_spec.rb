3ac840f823529ecc580e0b361c66d92ffd1aaccb
Guilherme Rojas V. de Lima + Rafael Reggiani Manzo <guilhermehrojas@gmail.com>
2013-09-04 11:47:28 -0300

    Refactored projects controller unit tests for update action

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -157,61 +157,78 @@ describe ProjectsController do
 
   describe 'update' do
     before do
-      @user = FactoryGirl.create(:user)
-
-      sign_in @user
+      @subject = FactoryGirl.build(:project)
+      @subject_params = Hash[FactoryGirl.attributes_for(:project).map { |k,v| [k.to_s, v.to_s] }] #FIXME: Mocha is creating the expectations with strings, but FactoryGirl returns everything with sybols and integers
     end
 
-    context 'with valid fields' do
-      before :each do
-        @subject = FactoryGirl.build(:project)
-        @subject_params = Hash[FactoryGirl.attributes_for(:project).map { |k,v| [k.to_s, v.to_s] }] #FIXME: Mocha is creating the expectations with strings, but FactoryGirl returns everything with sybols and integers
-        @ownership = FactoryGirl.build(:project_ownership)
-        @ownerships = []
-
-        @ownerships.expects(:find_by_project_id).with("#{@subject.id}").returns(@ownership)
-        User.any_instance.expects(:project_ownerships).at_least_once.returns(@ownerships)
-        Project.expects(:find).with(@subject.id.to_s).returns(@subject)
-        Project.any_instance.expects(:update).with(@subject_params).returns(true)
+    context 'when the user is logged in' do
+      before do
+        sign_in FactoryGirl.create(:user)
       end
 
-      context 'rendering the show' do
-        before :each do
-          Project.expects(:exists?).returns(true)
+      context 'when user owns the project' do
+        before do
+          @ownership = FactoryGirl.build(:project_ownership)
+          @ownerships = []
 
+          @ownerships.expects(:find_by_project_id).with("#{@subject.id}").returns(@ownership)
+          User.any_instance.expects(:project_ownerships).at_least_once.returns(@ownerships)
+        end
+
+        context 'with valid fields' do
+          before :each do
+            Project.expects(:find).with(@subject.id.to_s).returns(@subject)
+            Project.any_instance.expects(:update).with(@subject_params).returns(true)
+          end
+
+          context 'rendering the show' do
+            before :each do
+              Project.expects(:exists?).returns(true)
+
+              post :update, :id => @subject.id, :project => @subject_params
+            end
+
+            it 'should redirect to the show view' do
+              response.should redirect_to project_path(@subject)
+            end
+          end
+
+          context 'without rendering the show view' do
+            before :each do
+              post :update, :id => @subject.id, :project => @subject_params
+            end
+
+            it { should respond_with(:redirect) }
+          end
+        end
+
+        context 'with an invalid field' do
+          before :each do
+            Project.expects(:find).with(@subject.id.to_s).returns(@subject)
+            Project.any_instance.expects(:update).with(@subject_params).returns(false)
+
+            post :update, :id => @subject.id, :project => @subject_params
+          end
+
+          it { should render_template(:edit) }
+        end
+      end
+
+      context 'when the user does not own the project' do
+        before :each do
           post :update, :id => @subject.id, :project => @subject_params
         end
 
-        it 'should redirect to the show view' do
-          response.should redirect_to project_path(@subject)
-        end
-      end
-
-      context 'without rendering the show view' do
-        before :each do
-          post :update, :id => @subject.id, :project => @subject_params
-        end
-
-        it { should respond_with(:redirect) }
+        it { should redirect_to projects_path }
       end
     end
 
-    context 'with an invalid field' do
+    context 'with no user logged in' do
       before :each do
-        @subject = FactoryGirl.build(:project)
-        @subject_params = Hash[FactoryGirl.attributes_for(:project).map { |k,v| [k.to_s, v.to_s] }] #FIXME: Mocha is creating the expectations with strings, but FactoryGirl returns everything with sybols and integers
-        @ownership = FactoryGirl.build(:project_ownership)
-        @ownerships = []
-
-        @ownerships.expects(:find_by_project_id).with("#{@subject.id}").returns(@ownership)
-        User.any_instance.expects(:project_ownerships).at_least_once.returns(@ownerships)
-        Project.expects(:find).with(@subject.id.to_s).returns(@subject)
-        Project.any_instance.expects(:update).with(@subject_params).returns(false)
-
         post :update, :id => @subject.id, :project => @subject_params
       end
 
-      it { should render_template(:edit) }
+      it { should redirect_to new_user_session_path }
     end
   end
 
