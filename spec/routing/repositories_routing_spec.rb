7ed7d525004e6ebab67badeaf3d7507ae8ecf756
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2013-10-25 20:55:52 -0200

    Repositores routing specs

--- spec/routing/repositories_routing_spec.rb
+++ spec/routing/repositories_routing_spec.rb
@@ -2,6 +2,19 @@ require "spec_helper"
 
 describe RepositoriesController do
   describe "routing" do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it { should route(:post, '/projects/1/repositories').
+                  to(controller: :repositories, action: :create, project_id: 1) }
+    it { should route(:get, '/projects/1/repositories/new').
+                  to(controller: :repositories, action: :new, project_id: 1) }
+    it { should route(:get, '/projects/1/repositories/1/edit').
+                  to(controller: :repositories, action: :edit, project_id: 1, id: 1) }
+    it { should route(:get, '/projects/1/repositories/1').
+                  to(controller: :repositories, action: :show, project_id: 1, id: 1) }
+    it { should route(:delete, '/projects/1/repositories/1').
+                  to(controller: :repositories, action: :destroy, project_id: 1, id: 1) }
+    it { should route(:put, '/projects/1/repositories/1').
+                  to(controller: :repositories, action: :update, project_id: 1, id: 1) }
+    it { should_not route(:get, '/projects/1/repositories').
+                  to(controller: :repositories, action: :index, project_id: 1) }
   end
 end
