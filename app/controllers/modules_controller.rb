0980581e0e896933d010274f5323cda05efac02c
Renan Fichberg <rfichberg@gmail.com>
2013-11-11 17:21:11 -0200

    Ajax communication is now working.
    Signed-off By: Guilherme Rojas V. de Lima <guilhermehrojas@gmail.com>
    Signed-off By: Fellipe Souto Sampaio <fllsouto@gmail.com>

--- app/controllers/modules_controller.rb
+++ app/controllers/modules_controller.rb
@@ -1,8 +1,9 @@
+require 'json'
 class ModulesController < ApplicationController
-  
+
   # GET /modules/metric_history
   def metric_history
-    
+    render :json => {mk: "monkey"}.to_json
   end
 
 end
