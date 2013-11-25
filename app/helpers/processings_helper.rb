6f0673055806df6ed19cfd469aebb2c64694ff9a
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2013-11-25 13:03:38 -0200

    Fixed bug on processings_helper

--- app/helpers/processings_helper.rb
+++ app/helpers/processings_helper.rb
@@ -11,7 +11,7 @@ module ProcessingsHelper
     range_snapshots = metric_result.metric_configuration_snapshot.range_snapshot
 
     range_snapshots.each do |range_snapshot|
-      return range_snapshot if ((range_snapshot.beginning <= metric_result.value || range_snapshot.beginning = '-INF') && (range_snapshot.end >= metric_result.value || range_snapshot.beginning = '+INF'))
+      return range_snapshot if ((range_snapshot.beginning <= metric_result.value || range_snapshot.beginning == '-INF') && (range_snapshot.end >= metric_result.value || range_snapshot.beginning == '+INF'))
     end
 
     return nil
