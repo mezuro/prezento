401710c0501184c444ed7adc7d31e34d4f631d00
Diego Araújo <diegoamc90@gmail.com>
2014-01-27 12:52:09 -0200

    Schema with mezuro configuration ownerships.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219115819) do
+ActiveRecord::Schema.define(version: 20140124124835) do
+
+  create_table "mezuro_configuration_ownerships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "mezuro_configuration_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "project_ownerships", force: true do |t|
     t.integer  "user_id"
@@ -23,6 +30,12 @@ ActiveRecord::Schema.define(version: 20131219115819) do
   create_table "reading_group_ownerships", force: true do |t|
     t.integer  "user_id"
     t.integer  "reading_group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "repositories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
