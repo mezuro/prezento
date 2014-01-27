32178a640c04b2674095b5af0093db780774cc9f
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-01-27 13:05:35 -0200

    Removed INEXISTENT repositories table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -34,12 +34,6 @@ ActiveRecord::Schema.define(version: 20140124124835) do
     t.datetime "updated_at"
   end
 
-  create_table "repositories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
