fbbbdbd24720cc8a02cbc16c28676a0d50043f9e
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-02-10 13:57:49 -0200

    WIP deploy to NuvemUSP

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :user, 'mezuro'
 # RVM
 set :rvm_ruby_string, :local              # use the same ruby as used locally for deployment
 set :rvm_autolibs_flag, "read-only"       # more info: rvm help autolibs
-set :rvm_type, :user
+set :rvm_type, :system
 set :rvm_install_with_sudo, true
 
 namespace :deploy do
