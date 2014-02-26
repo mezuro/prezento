24d5f1189dd4d82951c67c801884f9bcd447f98a
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-02-26 19:41:47 -0300

    Fixed back behaviour for new NativeMetric

--- features/metric_configuration/create.feature
+++ features/metric_configuration/create.feature
@@ -27,3 +27,16 @@ Feature: Metric Configuration Creation
     Then I should see "My Code"
     Then I should see "Total Lines of Code"
     Then I should see "2"
+
+  @kalibro_restart @javascript
+  Scenario: metric configuration creation
+    Given I am a regular user
+    And I am signed in
+    And I own a sample configuration
+    And I have a reading group named "Scholar"
+    And I am at the Sample Configuration page
+    And I click the Add Metric link
+    And I click the "Analizo" h3
+    And I click the Total Lines of Code link
+    When I click the Back link
+    Then I should be at the choose metric page
--- features/step_definitions/metric_configuration_steps.rb
+++ features/step_definitions/metric_configuration_steps.rb
@@ -16,6 +16,10 @@ When(/^I visit the sample metric configuration page$/) do
   visit mezuro_configuration_metric_configuration_path(@metric_configuration.configuration_id, @metric_configuration.id)
 end
 
+When(/^I visit the sample metric configuration page$/) do
+  visit edit_mezuro_configuration_path(@mezuro_configuration.id)
+end
+
 Then(/^I am at the sample metric configuration page$/) do
   visit mezuro_configuration_metric_configuration_path(@metric_configuration.configuration_id, @metric_configuration.id)
   page.should have_content(@metric_configuration.metric.name)
@@ -28,12 +32,11 @@ Then(/^I should see the sample metric configuration content$/) do
   page.should have_content(@metric_configuration.weight)
 end
 
-When(/^I visit the sample metric configuration page$/) do
-  visit edit_mezuro_configuration_path(@mezuro_configuration.id)
-end
-
-
 Then(/^I should be at metric configuration sample page$/) do
   page.should have_content(@metric_configuration.metric.name)
   page.should have_content("Ranges")
+end
+
+Then(/^I should be at the choose metric page$/) do
+  page.should have_content("Choose a metric from a base tool:")
 end
