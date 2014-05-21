d143a65b83a29b026212d42b387d0e4061440992
Pedro Scocco <pedroscocco@gmail.com>
2014-05-21 15:35:36 -0300

    Added Unit tests for model validations
    signed-off-by: Fellipe Souto <fllsouto@gmail.com>

--- spec/models/mezuro_configuration_ownership_spec.rb
+++ spec/models/mezuro_configuration_ownership_spec.rb
@@ -10,12 +10,17 @@ describe MezuroConfigurationOwnership do
       subject {FactoryGirl.build(:mezuro_configuration_ownership)}
       let(:mezuro_configuration) {FactoryGirl.build(:mezuro_configuration)}
 
-      before :each do
-        MezuroConfiguration.expects(:find).with(subject.mezuro_configuration_id).returns(mezuro_configuration)
-      end
+      it { should validate_presence_of(:mezuro_configuration_id) }
 
       it 'should return the mezuro_configuration' do
+        MezuroConfiguration.expects(:find).with(subject.mezuro_configuration_id).returns(mezuro_configuration)
         subject.mezuro_configuration.should eq(mezuro_configuration)
+      end
+
+      it 'should fail to save with parent self' do
+        subject.parent = subject
+        subject.save
+        subject.errors[:base].should_not be_empty
       end
     end
   end
