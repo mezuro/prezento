2f7cdb3f2a92b3a6e4c89f4756b926e40c81b1d6
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2014-08-27 14:43:28 -0300

    Fixed range numericality unit tests and added beginning greater than end validation.
    Signed off by: Rafael Manzo <rr.manzo@gmail.com>

---
+++ app/models/validators/greater_than_beginning_validator.rb
@@ -0,0 +1,18 @@
+class GreaterThanBeginningValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if record.beginning.is_a?(String) || value.is_a?(String) #TOOO This will be useless when we start representing INF as ruby Infinity with the new Kalibro configuration application.
+      if record.beginning=="INF" || value=="-INF" || record.beginning == value
+        add_error(record,attribute)
+      end
+    elsif record.beginning >= value
+      add_error(record,attribute)
+    end
+  end
+
+  private
+
+  def add_error(record, attribute)
+    record.errors[attribute] << "The end value should be greater than the beginning value."
+  end
+
+end
