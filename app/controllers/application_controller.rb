0ef461ef64ecf145e863342e0270287a5aad9818
larissa <larissax55@gmail.com>
2014-10-20 20:22:24 -0200

    Creating method in controler for internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
   end
-
-  
 end
 
