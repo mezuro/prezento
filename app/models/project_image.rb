40028812e372f82ce9b6d9773e5faea68bc1c616
thiago <thiagitosouza@hotmail.com>
2014-10-27 10:52:14 -0200

    Fixed tests in project_controller.rb  and closes #25

--- app/models/project_image.rb
+++ app/models/project_image.rb
@@ -5,16 +5,16 @@ class ProjectImage < ActiveRecord::Base
 
   def check_no_image
     if !self.blank?
-       if self.image_url == "no-image-available.png"
-         self.image_url = ""
-       end
+      if self.image_url == "no-image-available.png"
+        self.image_url = ""
+      end
     end
   end
 
   def check_url
-  	if self.image_url.blank?
-        self.image_url = "no-image-available.png"
-      end
+    if self.image_url.blank?
+      self.image_url = "no-image-available.png"
+    end
   end
 
 
--- spec/factories/project_images.rb
+++ spec/factories/project_images.rb
@@ -1,7 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :project_image do
-    image_url "MyString"
-  end
+	factory :project_image do
+		project_id 1
+		image_url "Example"
+	end
+
+	factory :project_no_image, class: ProjectImage do
+		image_url nil
+	end
+
 end
