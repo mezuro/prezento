db1757f347fec26c75f1f8612bf0129d26bd0a0b
larissa <larissax55@gmail.com>
2014-11-01 16:48:06 -0200

    Inserting corrections in translations and translating other variables

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,7 @@ module Mezuro
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-<<<<<<< HEAD
     config.i18n.default_locale = :en
-=======
-    config.i18n.default_locale = :pt
->>>>>>> Correcting minor errors in translation files
     config.i18n.enforce_available_locales = false
   end
 end
