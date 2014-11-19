4360239ddca462e02b05f8191fc781e4015a7bd2
larissa <larissax55@gmail.com>
2014-11-19 19:43:43 -0200

    Bug fixes and a few more translations Signed-off-by: Vinicius Franco<viniciusf.arantes@gmail.com> Signed-off-by: Larissa Rodrigues<larissax55@gmail.com>

--- app/models/validators/kalibro_uniqueness_validator.rb
+++ app/models/validators/kalibro_uniqueness_validator.rb
@@ -2,7 +2,7 @@ class KalibroUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.class.all.each do |entity|
       if (entity.send(attribute) == value) and (entity.id != record.id)
-        record.errors[attribute] << "There is already a #{record.class} with #{attribute} #{value}! Please, choose another one."
+        record.errors[attribute] << t('kalibro_error_message', :record => t(record.class.name), :attribute => t(attribute.name), :value => value)
         break
       end
     end
