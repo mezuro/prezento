3c0473cef8d8958726640e5be0c3211d022d9860
Heitor Reis <marcheing@gmail.com>
2014-11-19 12:37:01 -0200

    Finished integration for Reading

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,9 +1,2 @@
 class Reading < KalibroClient::Configurations::Reading
-	include KalibroRecord
-
-  attr_accessor :label, :grade, :color
-
-  validates :label, presence: true, kalibro_uniqueness: true
-  validates :grade, presence: true, numericality: true
-  validates :color, presence: true
 end
