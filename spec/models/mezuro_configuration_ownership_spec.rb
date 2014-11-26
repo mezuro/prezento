39bedeeb234628d9d0c2341505dcf949a72c0f4c
Beatriz Rezener <beatrizrezener@gmail.com>
2014-11-25 22:56:43 -0200

    Removing useless methods from Configuration and ReadingGroup ownership model

--- spec/models/mezuro_configuration_ownership_spec.rb
+++ spec/models/mezuro_configuration_ownership_spec.rb
@@ -4,19 +4,4 @@ describe MezuroConfigurationOwnership, :type => :model do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
   end
-
-  describe 'methods' do
-    describe 'mezuro_configuration' do
-      subject {FactoryGirl.build(:mezuro_configuration_ownership)}
-      let(:mezuro_configuration) {FactoryGirl.build(:mezuro_configuration)}
-
-      before :each do
-        MezuroConfiguration.expects(:find).with(subject.mezuro_configuration_id).returns(mezuro_configuration)
-      end
-
-      it 'should return the mezuro_configuration' do
-        expect(subject.mezuro_configuration).to eq(mezuro_configuration)
-      end
-    end
-  end
 end
--- spec/models/reading_group_ownership_spec.rb
+++ spec/models/reading_group_ownership_spec.rb
@@ -4,19 +4,4 @@ describe ReadingGroupOwnership, :type => :model do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
   end
-
-  describe 'methods' do
-    describe 'reading_group' do
-      subject {FactoryGirl.build(:reading_group_ownership)}
-      let(:reading_group) {FactoryGirl.build(:reading_group)}
-
-      before :each do
-        ReadingGroup.expects(:find).with(subject.reading_group_id).returns(reading_group)
-      end
-
-      it 'should return the reading_group' do
-        expect(subject.reading_group).to eq(reading_group)
-      end
-    end
-  end
 end
