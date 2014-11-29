edf66a059dcf7cb885b42ad78b2481eb0896ec66
Vinicius Franco <viniciusf.arantes@gmail.com>
2014-11-28 22:05:25 -0200

    some cucumber tests fix

--- app/models/validators/kalibro_uniqueness_validator.rb
+++ app/models/validators/kalibro_uniqueness_validator.rb
@@ -2,7 +2,7 @@ class KalibroUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.class.all.each do |entity|
       if (entity.send(attribute) == value) and (entity.id != record.id)
-        record.errors[attribute] << t('kalibro_error_message', :record => t(record.class.name), :attribute => t(attribute.name), :value => value)
+        record.errors[attribute] << I18n.t('kalibro_error_message', :record => t(record.class.name), :attribute => t(attribute.name), :value => value)
         break
       end
     end
