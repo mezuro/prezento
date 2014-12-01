beee73e684774d2ee4bb0b018413aab2b27b6fe4
Heitor Reis <marcheing@gmail.com>
2014-12-01 14:58:19 -0200

    Integrated Module Result to Kalibro Client
    Signed off by: Daniel Alves <danpaulalves@gmail.com>

--- spec/factories/date_module_results.rb
+++ spec/factories/date_module_results.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :date_module_result, class: DateModuleResult do
     date "2011-10-20T18:26:43.151+00:00"
     module_result {FactoryGirl.build(:module_result)}
+
+    initialize_with { new({:date => date, :module_result => module_result}) }
   end
 end
--- spec/models/module_result_spec.rb
+++ spec/models/module_result_spec.rb
@@ -4,30 +4,16 @@ describe ModuleResult, :type => :model do
   describe 'methods' do
     subject { FactoryGirl.build(:module_result) }
     
-    describe 'metric_results' do
-      it 'should call the metric_results_of method' do
-        KalibroGatekeeperClient::Entities::MetricResult.expects(:metric_results_of).with(subject.id).returns(nil)
-
-        subject.metric_results
-      end
-    end
-
-    describe 'history' do
-      before :each do
-        ModuleResult.expects(:history_of).with(subject.id).returns([FactoryGirl.build(:date_module_result)])
-      end
-
-      it 'should return a array of DateModuleResults' do
-        expect(subject.history.first).to be_a(DateModuleResult)
-      end
-    end
-
     describe 'metric_history' do
       let(:date_module_result) {FactoryGirl.build(:date_module_result)}
       let(:metric_result) {FactoryGirl.build(:metric_result)}
+      let(:processing) {FactoryGirl.build(:processing)}
+      let(:repository) {FactoryGirl.build(:repository)}
 
       before :each do
-        subject.expects(:history).returns([date_module_result])
+        subject.expects(:processing).returns(processing)
+        processing.expects(:repository).returns(repository)
+        repository.expects(:module_result_history_of).with(subject).returns([date_module_result])
         ModuleResult.any_instance.expects(:metric_results).returns([metric_result])
       end
 
