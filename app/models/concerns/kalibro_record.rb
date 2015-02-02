7a3ac7d294af803ba3f4c558a0b7ab8cd908e959
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-02-02 16:22:43 -0200

    Updated kalibro_client and removed redundant deprecated persisted and update methods

--- app/models/concerns/kalibro_record.rb
+++ app/models/concerns/kalibro_record.rb
@@ -5,14 +5,4 @@ module KalibroRecord
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   delegate :url_helpers, to: 'Rails.application.routes'
-
-  def persisted?
-    self.class.exists?(self.id) unless self.id.nil?
-  end
-
-  #TODO maybe we don't need this method anymore
-  def update(attributes = {})
-    self.save
-  end
-
 end
