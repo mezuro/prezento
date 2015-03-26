c1eadf2d04485e90eecc4686bd079511ff94284d
Heitor Reis <marcheing@gmail.com>
2015-03-26 12:12:20 -0300

    Fixing routing tests for kalibro configurations helper

--- app/helpers/kalibro_configurations_helper.rb
+++ app/helpers/kalibro_configurations_helper.rb
@@ -5,17 +5,17 @@ module KalibroConfigurationsHelper
 
   def link_to_edit_form(metric_configuration, kalibro_configuration_id)
     if (metric_configuration.metric.is_a? KalibroClient::Entities::Miscellaneous::CompoundMetric)
-      link_to(t('edit'), edit_kalibro_configuration_compound_metric_configuration_path(kalibro_configuration_id, metric_configuration.id), class: 'btn btn-info')
+      link_to(t('edit'), edit_kalibro_configuration_compound_metric_configuration_path(kalibro_configuration_id: kalibro_configuration_id, id: metric_configuration.id), class: 'btn btn-info')
     else
-      link_to(t('edit'), edit_kalibro_configuration_metric_configuration_path(kalibro_configuration_id, metric_configuration.id), class: 'btn btn-info')
+      link_to(t('edit'), edit_kalibro_configuration_metric_configuration_path(kalibro_configuration_id: kalibro_configuration_id, id: metric_configuration.id), class: 'btn btn-info')
     end
   end
 
   def link_to_show_page(metric_configuration, kalibro_configuration_id)
     if (metric_configuration.metric.is_a? KalibroClient::Entities::Miscellaneous::CompoundMetric)
-      link_to(t('show'), kalibro_configuration_compound_metric_configuration_path(kalibro_configuration_id, metric_configuration.id), class: 'btn btn-info')
+      link_to(t('show'), kalibro_configuration_compound_metric_configuration_path(kalibro_configuration_id: kalibro_configuration_id, id: metric_configuration.id), class: 'btn btn-info')
     else
-      link_to(t('show'), kalibro_configuration_metric_configuration_path(kalibro_configuration_id, metric_configuration.id), class: 'btn btn-info')
+      link_to(t('show'), kalibro_configuration_metric_configuration_path(kalibro_configuration_id: kalibro_configuration_id, id: metric_configuration.id), class: 'btn btn-info')
     end
   end
 end
