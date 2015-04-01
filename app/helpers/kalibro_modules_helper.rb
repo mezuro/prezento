932883f3cabe5db60b133eb08140de3c820ce9e0
Diego Araújo <diegoamc90@gmail.com>
2015-04-01 16:45:18 -0300

    ModuleResult ordering done through Granularity built-in comparisons
    kalibro_client updated: It enables Granularity ordering
    
    Signed off by: Rafael Reggiani Manzo

--- app/helpers/kalibro_modules_helper.rb
+++ app/helpers/kalibro_modules_helper.rb
@@ -1,7 +1,11 @@
 module KalibroModulesHelper
   def sort_by_granularity_and_name(module_results)
-    module_results.sort do |a,b|
-      (a.kalibro_module.granularity == b.kalibro_module.granularity) ? a.kalibro_module.name <=> b.kalibro_module.name : -a.kalibro_module.granularity.length <=> -b.kalibro_module.granularity.length
+    module_results.sort! do |a,b|
+      if (a.kalibro_module.granularity == b.kalibro_module.granularity)
+        a.kalibro_module.name <=> b.kalibro_module.name
+      else
+        (KalibroClient::Entities::Miscellaneous::Granularity.new(b.kalibro_module.granularity.to_sym) <=> KalibroClient::Entities::Miscellaneous::Granularity.new(a.kalibro_module.granularity.to_sym))
+      end
     end
   end
 end
