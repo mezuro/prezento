86c063d00bc9543eab249f8856e686eaa9ce23f3
Diego Araújo <diegoamc90@gmail.com>
2015-04-10 18:11:59 -0300

    Translation for Count aggregation form
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- app/helpers/metric_configurations_helper.rb
+++ app/helpers/metric_configurations_helper.rb
@@ -1,6 +1,6 @@
 module MetricConfigurationsHelper
   def aggregation_options
-    [[t("aggregation_forms.AVERAGE"),"AVERAGE"], [t("aggregation_forms.MEDIAN"), "MEDIAN"], [t("aggregation_forms.MAXIMUM"), "MAXIMUM"], [t("aggregation_forms.MINIMUM"), "MINIMUM"], [t("aggregation_forms.STANDARD_DEVIATION"), "STANDARD_DEVIATION"], ["Count", "COUNT"]]
+    [[t("aggregation_forms.AVERAGE"),"AVERAGE"], [t("aggregation_forms.MEDIAN"), "MEDIAN"], [t("aggregation_forms.MAXIMUM"), "MAXIMUM"], [t("aggregation_forms.MINIMUM"), "MINIMUM"], [t("aggregation_forms.STANDARD_DEVIATION"), "STANDARD_DEVIATION"], [t("aggregation_forms.COUNT"), "COUNT"]]
   end
 
   def reading_group_options
