c3cd29f7c9f74cf3eefe8a3f92ebad9c8cb3eb5e
Heitor Reis <marcheing@gmail.com>
2015-04-17 15:05:52 -0300

    Miscellaneous translations, reviewing for #204
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def t_hint(attribute_key, class_key=nil)
+  def t_hint(attribute_key, class_key=nil, options={})
     class_key ||= controller_name.singularize
-    t("activemodel.hints.#{class_key}.#{attribute_key}")
+    t("activemodel.hints.#{class_key}.#{attribute_key}", options)
   end
 end
