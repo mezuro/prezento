a4a40c576fbfa3fce52ea00cf9e766db7bcc87b6
Daniel Miranda <danielkza2@gmail.com>
2015-05-04 17:15:49 -0300

    Add helper for translating actions for models

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     class_key ||= controller_name.singularize
     t("activemodel.hints.#{class_key}.#{attribute_key}", options)
   end
+
+  def t_action(action, model, options={})
+    options[:default] = "helpers.submit.#{action}".to_sym
+    count = options.delete(:count) || 1
+    options[:model] = model.model_name.human(count: count)
+
+    t("helpers.submit.#{model.model_name.i18n_key}.#{action}", options)
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -23,4 +23,21 @@ describe ApplicationHelper, :type => :helper do
       end
     end
   end
+
+  describe 't_action' do
+    let!(:model) { mock "Model" }
+    let!(:model_name) { mock "Model Name" }
+    let(:action) { :edit }
+
+    before :each do
+      model.expects(:model_name).twice.returns(model_name)
+      model_name.expects(:i18n_key).returns(:model)
+      model_name.expects(:human).with(count: 1).returns("Model")
+      helper.expects(:t).with("helpers.submit.model.edit", {default: "helpers.submit.edit".to_sym, model: "Model"}).returns("Edit Model")
+    end
+
+    it "is expected to return the Model's edit action translation" do
+      expect(helper.t_action(action, model)).to eq("Edit Model")
+    end
+  end
 end
