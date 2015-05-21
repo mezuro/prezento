f87124d3fde9da948a4371ff54d1430bdfd9f205
Álvaro Fernando <alvarofernandoms@gmail.com>
2015-05-21 15:56:08 -0300

    Init rails_admin setup
    Signed-off-by: Álvaro Fernando <alvarofernandoms@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   scope "(:locale)", locale: /en|pt/ do
     devise_for :users
     get 'users/:user_id/projects' => 'users#projects', as: :user_projects
