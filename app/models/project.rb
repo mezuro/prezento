937d18284547e3eb5267698c32a939586cf85606
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-05-22 08:39:32 -0300

    Project#attributes memoization
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,14 @@
 class Project < KalibroClient::Entities::Processor::Project
   include KalibroRecord
 
+  attr_writer :attributes
+
   def self.latest(count = 1)
     all.sort { |a,b| b.id <=> a.id }.select { |project| !project.attributes.hidden}.first(count)
   end
 
   def attributes
-    project_attributes = ProjectAttributes.find_by_project_id(self.id)
-    project_attributes.nil? ? ProjectAttributes.new : project_attributes
+    @project_attributes ||= ProjectAttributes.find_by_project_id(self.id)
+    @project_attributes.nil? ? ProjectAttributes.new : @project_attributes
   end
 end
