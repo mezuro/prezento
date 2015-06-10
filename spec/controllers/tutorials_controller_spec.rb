9bc3ce8558cb310e4d88ab32ffa75e833c6b3802
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-06-10 13:04:39 -0300

    Handle invalid tutorial names

--- spec/controllers/tutorials_controller_spec.rb
+++ spec/controllers/tutorials_controller_spec.rb
@@ -2,13 +2,25 @@ require 'rails_helper'
 
 describe TutorialsController do
   describe 'view' do
-    let!(:name) { "analyzing" }
+    context 'with a valid name' do
+      let!(:name) { "analyzing" }
 
-    before :each do
-      get :view, name: name
+      before :each do
+        get :view, name: name
+      end
+
+      it { is_expected.to render_template(name) }
+      it { is_expected.to respond_with(:success) }
     end
 
-    it { is_expected.to render_template(name) }
-    it { is_expected.to respond_with(:success) }
+    context 'with a invalid name' do
+      let!(:name) { "invalid_name" }
+
+      before :each do
+        get :view, name: name
+      end
+
+      it { is_expected.to respond_with(:not_found) }
+    end
   end
 end
