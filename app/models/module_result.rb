8bf9a57a9e7a1cc1237c454d47834d381108e4af
Heitor Reis <marcheing@gmail.com>
2015-10-01 14:21:01 -0300

    Rename MetricResult to TreeMetricResult on unit tests
     * Renamed the classes, mocks and factory class on unit tests
    
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- app/models/module_result.rb
+++ app/models/module_result.rb
@@ -1,6 +1,6 @@
 class ModuleResult < KalibroClient::Entities::Processor::ModuleResult
   def metric_history(name)
-    history = KalibroClient::Entities::Processor::MetricResult.history_of(name, self.id, processing.repository_id)
+    history = KalibroClient::Entities::Processor::TreeMetricResult.history_of(name, self.id, processing.repository_id)
     grade_history = Hash.new
 
     history.each { |date_metric_result| grade_history[date_metric_result.date] = date_metric_result.metric_result.value }
--- spec/models/module_result_spec.rb
+++ spec/models/module_result_spec.rb
@@ -4,7 +4,7 @@ describe ModuleResult, :type => :model do
   describe 'methods' do
     subject { FactoryGirl.build(:module_result) }
 
-    describe 'metric_history', pending: 'broke after kalibro client major update' do
+    describe 'metric_history' do
       let(:date_module_result) {FactoryGirl.build(:date_module_result)}
       let(:metric_configuration) { FactoryGirl.build(:another_metric_configuration_with_id) }
       let!(:metric_result) { FactoryGirl.build(:metric_result, metric_configuration: metric_configuration) }
@@ -13,7 +13,7 @@ describe ModuleResult, :type => :model do
       before :each do
         subject.expects(:processing).returns(processing)
         date_module_result.expects(:metric_result).returns(metric_result)
-        KalibroClient::Entities::Processor::MetricResult.expects(:history_of).with(metric_configuration.metric.name, subject.id, processing.repository_id).returns([date_module_result])
+        KalibroClient::Entities::Processor::TreeMetricResult.expects(:history_of).with(metric_configuration.metric.name, subject.id, processing.repository_id).returns([date_module_result])
       end
 
       it 'should return the history for the given metric name' do
