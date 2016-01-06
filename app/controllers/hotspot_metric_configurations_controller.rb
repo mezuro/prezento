c91af6365361a18f426c6fc06d7fd78b79f1cdd7
Heitor Reis <marcheing@gmail.com>
2016-01-06 16:15:38 -0200

    Add a redirect on hotspot metric configuration creation
    Now it doesn't redirect to the tree metric configuration creation page
    after choosing a hotspot metric configuration
    
    Adds a bunch of code from removed commit
    ec4c3ef6f29aa34f3c58819df4f84725aff8bd60 that fixed the view after the
    modification introduced by 39bc97f568d6989ae98b1f5fd5452515ee54045f.
    
    It fixes the view to properly work with the path helper.
    
    This is a squash of two commits.
    
    Signed off by: Diego Araújo <diegoamc90@gmail.com>
    Signed off by: Heitor Reis <marcheing@gmail.com>

--- app/controllers/hotspot_metric_configurations_controller.rb
+++ app/controllers/hotspot_metric_configurations_controller.rb
@@ -1,3 +1,41 @@
 class HotspotMetricConfigurationsController < BaseMetricConfigurationsController
-  def create; end
+  def create
+    super
+    @metric_configuration.metric = KalibroClient::Entities::Processor::MetricCollectorDetails.find_by_name(params[:metric_collector_name]).find_metric_by_code params[:metric_code]
+    respond_to do |format|
+      if @metric_configuration.save
+        format.html do
+          redirect_to kalibro_configuration_path(@metric_configuration.kalibro_configuration_id),
+            notice: t('successfully_created', record: t(@metric_configuration.class))
+        end
+      else
+        failed_action(format)
+      end
+    end
+    clear_caches
+  end
+
+  protected
+
+  def update_metric_configuration(new_metric_configuration)
+    @kalibro_configuration_id = params[:kalibro_configuration_id]
+    @metric_configuration = new_metric_configuration
+  end
+
+  def metric_configuration_params
+    { kalibro_configuration_id: params[:kalibro_configuration_id] }
+  end
+
+  private
+
+  def failed_action(format)
+    @kalibro_configuration_id = params[:kalibro_configuration_id]
+    format.html { redirect_to kalibro_configuration_choose_metric_path }
+    format.json { render json: @metric_configuration.kalibro_errors, status: :unprocessable_entity }
+  end
+
+  def clear_caches
+    Rails.cache.delete("#{params[:kalibro_configuration_id]}_tree_metric_configurations")
+    Rails.cache.delete("#{params[:kalibro_configuration_id]}_hotspot_metric_configurations")
+  end
 end
