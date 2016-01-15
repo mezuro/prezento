eaa33fc40d8149e9a271e3f7675a2276c2c83014
Heitor Reis <marcheing@gmail.com>
2016-01-15 16:14:42 -0200

    Refactor MetricConfiguration controllers
    Move as much code as possible to BaseMetricConfigurationsController
    instead of repeating it ad-hoc in the concrete classes. Most of the
    concrete code has gone away except for some small overrides:
    
    - Hotspot's does not have a 'new' action, and therefore can't redirect
    to it. It also does not accept any MetricConfiguration parameters.
    - Metric's only accepts 'weight' and 'aggregation_form'
    MetricConfiguration parameters. It also needs to fetch ReadingGroups to
    display in the 'choose_metric' action.
    - Compound's needs to force the Metric type when creating the metric,
    and must accept parameters for Metric ('name', 'description', 'script',
    'scope', 'code') in addition to MetricConfiguration's 'weight'.
    
    Additionally:
    - Update cache clearing to clear caches for Hotspot and Tree
    configurations
    - Verify the existence of the KalibroConfiguration and use the object
    that was fetched whenever necessary, instead of only using the id.
    - Ensure the KalibroConfiguration and ReadingGroup ids match between the
    values inside and outside the 'metric_configuration' parameter.
    Previously it was possible to send one in the URL and update another,
    which might even have been a security issue.
    - Allow sending either 'metric_code' or 'metric_name' as a parameter.
    Both are used in different places in the code, but making them uniform
    is a job for a later time.
    
    Signed off by: Daniel Miranda <danielkza2@gmail.com>

--- app/controllers/hotspot_metric_configurations_controller.rb
+++ app/controllers/hotspot_metric_configurations_controller.rb
@@ -1,41 +1,15 @@
 class HotspotMetricConfigurationsController < BaseMetricConfigurationsController
-  def create
-    super
-    @metric_configuration.metric = KalibroClient::Entities::Processor::MetricCollectorDetails.find_by_name(params[:metric_collector_name]).find_metric_by_code params[:metric_code]
-    respond_to do |format|
-      if @metric_configuration.save
-        format.html do
-          redirect_to kalibro_configuration_path(@metric_configuration.kalibro_configuration_id),
-            notice: t('successfully_created', record: t(@metric_configuration.class))
-        end
-      else
-        failed_action(format)
-      end
-    end
-    clear_caches
-  end
-
-  protected
-
-  def update_metric_configuration(new_metric_configuration)
-    @kalibro_configuration_id = params[:kalibro_configuration_id]
-    @metric_configuration = new_metric_configuration
-  end
+  METRIC_TYPE = 'HotspotMetricSnapshot'
 
   def metric_configuration_params
-    { kalibro_configuration_id: params[:kalibro_configuration_id] }
+    # Hotspot metric configurations don't accept any parameters on creation
+    # But we must make that explicit as the method isn't implemented in the parent class
+    params.permit
   end
 
-  private
-
-  def failed_action(format)
-    @kalibro_configuration_id = params[:kalibro_configuration_id]
-    format.html { redirect_to kalibro_configuration_choose_metric_path }
+  # FIXME: This action should render with an error message on rendering the html
+  def failed_action(format, _)
+    format.html { redirect_to kalibro_configurations_path(@kalibro_configuration.id) }
     format.json { render json: @metric_configuration.kalibro_errors, status: :unprocessable_entity }
-  end
-
-  def clear_caches
-    Rails.cache.delete("#{params[:kalibro_configuration_id]}_tree_metric_configurations")
-    Rails.cache.delete("#{params[:kalibro_configuration_id]}_hotspot_metric_configurations")
   end
 end
--- app/controllers/metric_configurations_controller.rb
+++ app/controllers/metric_configurations_controller.rb
@@ -1,91 +1,29 @@
 class MetricConfigurationsController < BaseMetricConfigurationsController
-  def choose_metric
-    @kalibro_configuration = KalibroConfiguration.find(params[:kalibro_configuration_id].to_i)
-    @metric_configuration_id = params[:metric_configuration_id].to_i
-    @metric_collectors_names = KalibroClient::Entities::Processor::MetricCollectorDetails.all_names
-  end
+  METRIC_TYPE = 'NativeMetricSnapshot'
+
+  before_action :set_reading_groups!, only: [:new, :edit]
 
   def new
     super
-    # FIXME: find_by_name throws an exception instead of returning nil, unlike ActiveRecord's API
-    metric_configuration.metric = KalibroClient::Entities::Processor::MetricCollectorDetails.find_by_name(params[:metric_collector_name]).find_metric_by_code params[:metric_code]
-    @reading_groups = ReadingGroup.public_or_owned_by_user(current_user).map { |reading_group|
-      [reading_group.name, reading_group.id]
-    }
+    # The metric parameter comes from the choose metric form
+    set_metric!
   end
 
-  def create
-    super
-    @metric_configuration.metric = KalibroClient::Entities::Processor::MetricCollectorDetails.find_by_name(params[:metric_collector_name]).find_metric_by_name params[:metric_name]
-    respond_to do |format|
-      create_and_redir(format)
-    end
-    clear_caches
-  end
-
-  def edit
-    # FIXME: set the configuration id just once!
-    @kalibro_configuration_id = params[:kalibro_configuration_id]
-    @metric_configuration.kalibro_configuration_id = @kalibro_configuration_id
-    @reading_groups = ReadingGroup.public_or_owned_by_user(current_user).map { |reading_group|
-      [reading_group.name, reading_group.id]
-    }
-  end
-
-  def update
-    respond_to do |format|
-      @metric_configuration.kalibro_configuration_id = params[:kalibro_configuration_id]
-      if @metric_configuration.update(metric_configuration_params)
-        format.html { redirect_to(kalibro_configuration_path(@metric_configuration.kalibro_configuration_id), notice: t('successfully_updated', :record => t(metric_configuration.class))) }
-        format.json { head :no_content }
-        clear_caches
-      else
-        failed_action(format, 'edit')
-      end
-    end
-  end
-
-  def destroy
-    @metric_configuration.destroy
-    respond_to do |format|
-      format.html { redirect_to kalibro_configuration_path(params[:kalibro_configuration_id]) }
-      format.json { head :no_content }
-    end
-    clear_caches
+  def choose_metric
+    @metric_collectors_names = KalibroClient::Entities::Processor::MetricCollectorDetails.all_names
   end
 
   protected
 
-  def metric_configuration
-    @metric_configuration
-  end
-
-  def update_metric_configuration (new_metric_configuration)
-    @kalibro_configuration_id = params[:kalibro_configuration_id]
-    @metric_configuration = new_metric_configuration
+  def metric_configuration_params
+    params.require(:metric_configuration).permit(:reading_group_id, :weight, :aggregation_form)
   end
 
   private
 
-  def clear_caches
-    Rails.cache.delete("#{params[:kalibro_configuration_id]}_tree_metric_configurations")
-    Rails.cache.delete("#{params[:kalibro_configuration_id]}_hotspot_metric_configurations")
-  end
-
-  # FIXME: Duplicated code on create and update actions extracted here
-  def failed_action(format, destiny_action)
-    @kalibro_configuration_id = params[:kalibro_configuration_id]
-
-    format.html { render action: destiny_action }
-    format.json { render json: @metric_configuration.kalibro_errors, status: :unprocessable_entity }
-  end
-
-  # Code extracted from create action
-  def create_and_redir(format)
-    if @metric_configuration.save
-      format.html { redirect_to kalibro_configuration_path(@metric_configuration.kalibro_configuration_id), notice: t('successfully_created', :record => t(metric_configuration.class)) }
-    else
-      failed_action(format, 'new')
+  def set_reading_groups!
+    @reading_groups = ReadingGroup.public_or_owned_by_user(current_user).map do |reading_group|
+      [reading_group.name, reading_group.id]
     end
   end
 end
