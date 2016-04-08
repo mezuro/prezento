bcd6ba1814b7a077884abcf253042233fdbbdc3e
Eduardo Silva Araújo <duduktamg@hotmail.com>
2016-04-08 14:18:26 -0300

    Add FUNCTION scope to compound_metric_configuration form
    Signed-off-by: Daniel Miranda <danielkza2@gmail.com>

--- spec/helpers/compound_metric_configurations_helper_spec.rb
+++ spec/helpers/compound_metric_configurations_helper_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe CompoundMetricConfigurationsHelper, :type => :helper do
   describe 'scope_options' do
     it 'should return an array with the supported scope options' do
-      expect(helper.scope_options).to eq [["Method","METHOD"], ["Class", "CLASS"], ["Package", "PACKAGE"], ["Software", "SOFTWARE"]]
+      expect(helper.scope_options).to eq [
+         ["Function", "FUNCTION"], ["Method","METHOD"], ["Class", "CLASS"], ["Package", "PACKAGE"], ["Software", "SOFTWARE"]
+      ]
     end
   end
 
