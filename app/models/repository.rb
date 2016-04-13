a9dd14ea80280dff7036a4930dfbd042cee36372
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-04-13 16:33:38 -0300

    Create latest Repositories listing method
    This is a information that will get published at the homepage.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,7 @@
 class Repository < KalibroClient::Entities::Processor::Repository
   include KalibroRecord
+
+  def self.latest(count=1)
+    all.sort { |one, another| another.id <=> one.id }.first(count)
+  end
 end
