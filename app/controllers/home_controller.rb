610763c4f6a22b9db9bcf044a0fa990a50ded422
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-04-13 16:53:47 -0300

    Add latest Repositories and Configurations to home
    The acceptance test has been refactored accordingly and got more
    extended to untested features.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @latest_projects = Project.latest(5)
+    @latest_repositories = Repository.latest(5)
+    @latest_configurations = KalibroConfiguration.latest(5)
   end
 end
