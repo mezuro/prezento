801561148a761399be4a9bea6ee0d828109cdb6b
Diego Araújo <diegoamc@protonmail.ch>
2016-04-18 16:42:31 -0300

    Implement Repository#public_or_owned_by_user
    This method is a copy and paste from other methods (of Project, for
    example) and should be refactored to avoid code duplications.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,19 @@
 class Repository < KalibroClient::Entities::Processor::Repository
   include KalibroRecord
 
+  def self.public_or_owned_by_user(user = nil)
+    repository_attributes = RepositoryAttributes.where(public: true)
+    repository_attributes += RepositoryAttributes.where(user_id: user.id, public: false) if user
+
+    repository_attributes.map do |attribute|
+      begin
+        self.find(attribute.repository_id)
+      rescue Likeno::Errors::RecordNotFound
+        nil
+      end
+    end.compact
+  end
+
   def self.latest(count=1)
     all.sort { |one, another| another.id <=> one.id }.first(count)
   end
--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -20,5 +20,62 @@ describe Repository do
         end
       end
     end
+
+    describe 'public_or_owned_by_user' do
+      let!(:user) { FactoryGirl.build(:user, :with_id) }
+
+      let!(:owned_private_attrs)     { FactoryGirl.build(:repository_attributes, :private, user_id: user.id) }
+      let!(:owned_public_attrs)      { FactoryGirl.build(:repository_attributes,           user_id: user.id) }
+      let!(:not_owned_private_attrs) { FactoryGirl.build(:repository_attributes, :private, user_id: user.id + 1) }
+      let!(:not_owned_public_attrs)  { FactoryGirl.build(:repository_attributes,           user_id: user.id + 1) }
+
+      let!(:public_attrs) { [owned_public_attrs, not_owned_public_attrs] }
+      let(:public_repositories) { public_attrs.map(&:repository) }
+
+      let!(:owned_or_public_attrs) { public_attrs + [owned_private_attrs] }
+      let!(:owned_or_public_repositories) { owned_or_public_attrs.map(&:repository) }
+
+      let(:all_repositories) { owned_or_public_repositories + [not_owned_private_attrs.repository] }
+
+      context 'when repositories exist' do
+        before :each do
+          all_repositories.each do |repository|
+            described_class.stubs(:find).with(repository.id).returns(repository)
+          end
+
+          RepositoryAttributes.expects(:where).with(public: true).returns(public_attrs)
+        end
+
+        context 'when user is not provided' do
+          it 'is expected to find all public repositories' do
+            expect(described_class.public_or_owned_by_user).to eq(public_repositories)
+          end
+        end
+
+        context 'when user is provided' do
+          before do
+            RepositoryAttributes.expects(:where).with(user_id: user.id, public: false).returns([owned_private_attrs])
+          end
+
+          it 'is expected to find all public and owned repositories' do
+            expect(described_class.public_or_owned_by_user(user)).to eq(owned_or_public_repositories)
+          end
+        end
+      end
+
+      context 'when no repositories exist' do
+        before :each do
+          all_repositories.each do |repository|
+            described_class.stubs(:find).with(repository.id).raises(Likeno::Errors::RecordNotFound)
+          end
+
+          RepositoryAttributes.expects(:where).with(public: true).returns(public_attrs)
+        end
+
+        it 'is expected to be empty' do
+          expect(described_class.public_or_owned_by_user).to be_empty
+        end
+      end
+    end
   end
 end
