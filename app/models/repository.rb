c1d56fd0a5d702ad2b31d337777f5d79a8fd5a02
Wilton Rodrigues <braynwilton@gmail.com>
2016-08-23 19:07:21 -0300

    Fixing issue #347
    Adding a method to remove the repository's instance that has remained in the
    db.
    
    Signed-off-by: Wilton Rodrigues <braynwilton@gmail.com>
    Signed-off-by: Jonnatas Lennon <jonatas_lenon@hotmail.com>

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -23,4 +23,10 @@ class Repository < KalibroClient::Entities::Processor::Repository
   def attributes
     @attributes ||= RepositoryAttributes.find_by_repository_id(@id)
   end
+
+  def destroy
+    self.attributes.destroy unless self.attributes.nil?
+    @attributes = nil
+    super
+  end
 end
