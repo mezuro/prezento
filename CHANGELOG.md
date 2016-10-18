1590cd81a14f0676ab4acf374a72cfc16e971eb4
Marcos Dourado <marcosdourado92@gmail.com>
2016-10-18 19:06:53 -0200

    refactor latest method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The version numbers below try to follow the conventions at http://semver.org/.
 
 ## Unreleased
 
+- Refactor latest method
 - Remove the repository's instance that has remained in the db
 - Adding translation of the periodicity options in repository helper
 - Fixing wrong configurations translation in portuguese navbar
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,13 +4,6 @@ class Project < KalibroClient::Entities::Processor::Project
 
   attr_writer :attributes
 
-  def self.latest(count = 1)
-    all.sort { |one, another| another.id <=> one.id }.select { |project|
-      attributes = project.attributes
-      attributes && attributes.public
-    }.first(count)
-  end
-
   def attributes
     @attributes ||= ProjectAttributes.find_by_project_id(@id)
   end
