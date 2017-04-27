e1cfeb0cb35bdab9ede7b6e6fa1f7850cec8f8c9
Lucas Amoêdo <lucas_advc@outlook.com>
2017-04-27 17:50:48 -0300

    Add gravatar to users
    Related to issue #398
    
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>
    Signed-off-by: Pedro Ivo <landradepedroivo@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+	include Gravtastic
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
+
+	has_gravatar
 
   has_many :project_attributes, class_name: 'ProjectAttributes', dependent: :destroy
   has_many :reading_group_attributes, class_name: 'ReadingGroupAttributes', dependent: :destroy
